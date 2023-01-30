3d5bbe920c8c27ab933dc6bd3e56a7e2230f44cf
Dilshod1421 <dilshod7861@gmail.com>
2023-01-30 22:43:47 +0500

    middleware routes

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+import express from 'express';
+import { create } from 'express-handlebars';
+import AuthRoutes from './routes/auth.js';
+import ProductsRoutes from './routes/products.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const hbs = exphbs.create({
+const hbs = create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
-app.set('views', 'views');
+app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('index', {});
-});
-
-app.get('/about', (req, res) => {
-    res.render('about', {});
-});
+app.use(AuthRoutes);
+app.use(ProductsRoutes);
 
 const port = process.env.PORT || 1212;
 app.listen(port, () => console.log(`Server listening on ${port}`));
