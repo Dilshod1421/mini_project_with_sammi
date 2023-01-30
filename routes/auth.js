093f8f565178c32068759d3210c072b4f39f68be
Dilshod1421 <dilshod7861@gmail.com>
2023-01-30 23:02:26 +0500

    dynamic title

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,23 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Shop | AbuDev'
+    });
 });
 
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {
+        title: 'Login | AbuDev',
+        isLogin: true
+    });
 });
 
 router.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', {
+        title: 'Register | AbuDev',
+        isRegister: true
+    });
 });
 
 export default router;
--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,17 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/add', (req, res) => {
-    res.render('add');
+    res.render('add', {
+        title: 'Add | AbuDev',
+        isAdd: true
+    });
 });
 
 router.get('/products', (req, res) => {
-    res.render('products');
+    res.render('products', {
+        title: 'Products | AbuDev',
+        isProducts: true
+    });
 });
 
 export default router;
