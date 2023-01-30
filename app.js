c155605772fadde25cfa8997ba48f8131add7e85
Dilshod1421 <dilshod7861@gmail.com>
2023-01-30 21:47:29 +0500

    layout

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.get('/', (req, res) => {
+    res.render('index', {});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {});
+});
+
+const port = process.env.PORT || 1212;
+app.listen(port, () => console.log(`Server listening on ${port}`));
